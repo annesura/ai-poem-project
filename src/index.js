b48ff28d405b60f25e58293abb1aefa86864f455
annesura <annesura00@gmail.com>
2025-01-17 17:43:31 -0500

    Added api and js to poem generator

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 function generatePoem(event) {
     event.preventDefault();
 
-    new Typewriter("#poem", {
-        strings: "Dainty petals, fancy flair",
+    let userTopic = document.querySelector("#user-topic");
+    let apiKey = "0cca9cddf1f4t4bb307e8bfo1fa213f2";
+    let context = `You are a poetic AI assistant that loves to share four lined poems. Please write the response in basic HTML paragraphs with ONLY four lines of code`;
+    let prompt = `Generate a short and sweet poem about ${userTopic.value}`;
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+    
+    axios.get(apiUrl).then(displayPoem);
+    
+    console.log("generating poem...");
+}
+
+function displayPoem(response) {
+console.log("poem generated");
+
+ new Typewriter("#poem", {
+        strings: response.data.answer,
         autoStart: true,
         delay: 1,
+        cursor: "",
       });
+
 }
 
 let poemForm = document.querySelector("#poem-form");
 poemForm.addEventListener("submit", generatePoem);
+
