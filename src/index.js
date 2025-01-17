04017f1e660118d82ca601e4a8ad9f1777201f88
annesura <annesura00@gmail.com>
2025-01-17 18:38:15 -0500

    Worked on User Experience
    Added a loading element, removed console.logs

--- src/index.js
+++ src/index.js
@@ -2,26 +2,26 @@ function generatePoem(event) {
     event.preventDefault();
 
     let userTopic = document.querySelector("#user-topic");
+    let poemDisplay = document.querySelector("#poem");
     let apiKey = "0cca9cddf1f4t4bb307e8bfo1fa213f2";
-    let context = `You are a poetic AI assistant that loves to share four lined poems. Please write the response in basic HTML paragraphs with ONLY four lines of code`;
+    let context = `You are a poetic AI assistant that loves to share poems. Please write the response in basic HTML, separating each line with <br /> and with ONLY four lines. Do not include any html elements in the display`;
     let prompt = `Generate a short and sweet poem about ${userTopic.value}`;
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
     
     axios.get(apiUrl).then(displayPoem);
-    
-    console.log("generating poem...");
+    poemDisplay.classList.remove("hidden");
+    poemDisplay.innerHTML = `Generating your poem...`;
 }
 
 function displayPoem(response) {
-console.log("poem generated");
 
- new Typewriter("#poem", {
-        strings: response.data.answer,
-        autoStart: true,
-        delay: 1,
-        cursor: "",
-      });
 
+new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
 }
 
 let poemForm = document.querySelector("#poem-form");
